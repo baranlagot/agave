ee92d83c5ac7be1fb9be0e3d99c794ffe014c6d2
baranlagot <103458360+baranlagot@users.noreply.github.com>
2025-02-04 01:40:31 +0300

    refactor board  manager

--- Assets/Scripts/GameLogic/BoardManager.cs
+++ Assets/Scripts/GameLogic/BoardManager.cs
@@ -7,30 +7,45 @@ using UnityEngine;
 public class BoardManager : MonoBehaviour, ICommandContext
 {
     [SerializeField] private GameData gameData;
-    private int width => gameData.boardWidth;
-    private int height => gameData.boardHeight;
     [SerializeField] private BoardItemView boardItemViewPrefab;
     [SerializeField] private GameObject backgroundTilePrefab;
     [SerializeField] private FeedbackPlayer feedbackPlayer;
     [SerializeField] private ParticleFeedback particleFeedback;
 
     private Dictionary<(int, int), BoardItemView> boardItemViews = new Dictionary<(int, int), BoardItemView>();
-
     private IBoardItemViewFactory boardItemViewFactory;
     private Board board;
 
+    // Configuration
     private float boardItemSize = 2.5f;
-
     private float xOffset => (width * boardItemSize) / 2f - boardItemSize / 2f;
     private float yOffset => (height * boardItemSize) / 2f - boardItemSize / 2f;
+    private int width => gameData.boardWidth;
+    private int height => gameData.boardHeight;
+
+    #region Initialization
 
     private void Awake()
     {
+        InitializeBoard();
+        InitializeVisuals();
+    }
+
+    private void InitializeBoard()
+    {
         board = new Board(width, height);
         board.InitializeRandom();
+    }
+
+    private void InitializeVisuals()
+    {
         boardItemViewFactory = new BoardItemViewFactory(boardItemViewPrefab, width * height, transform);
         CreateVisualsFromBoard();
     }
+
+    #endregion
+
+    #region Board Visualization
 
     private void CreateVisualsFromBoard()
     {
@@ -39,21 +54,22 @@ public class BoardManager : MonoBehaviour, ICommandContext
         {
             for (int y = 0; y < height; y++)
             {
+                CreateBackgroundImageAt(x, y);
                 var cell = board.GetCell(x, y);
                 if (!cell.IsEmpty)
                 {
                     CreateBoardItemViewAt(x, y, cell.boardItem);
                 }
-                CreateBackgroundImageAt(x, y);
             }
         }
     }
 
     private BoardItemView CreateBoardItemViewAt(int x, int y, IBoardItem boardItem)
     {
-        var boardItemView = boardItemViewFactory.CreateBoardItem(boardItem, GetCellWorldPosition(x, y));
-        boardItemViews[(x, y)] = boardItemView as BoardItemView;
-        return boardItemView as BoardItemView;
+        var position = GetCellWorldPosition(x, y);
+        var boardItemView = boardItemViewFactory.CreateBoardItem(boardItem, position) as BoardItemView;
+        boardItemViews[(x, y)] = boardItemView;
+        return boardItemView;
     }
 
     private void CreateBackgroundImageAt(int x, int y)
@@ -62,11 +78,147 @@ public class BoardManager : MonoBehaviour, ICommandContext
         Instantiate(backgroundTilePrefab, worldPosition, Quaternion.identity, transform);
     }
 
-    private IEnumerator DelayRoutine(float delayAmount, Action action)
+    internal Vector3 GetCellWorldPosition(int x, int y)
     {
-        yield return new WaitForSeconds(delayAmount);
-        action?.Invoke();
+        return new Vector3(x * boardItemSize - xOffset, y * boardItemSize - yOffset, -y);
     }
+
+    #endregion
+
+    #region Board State Management
+
+    public Board GetBoard() => board;
+
+    public BoardCell? GetBoardCellFromMousePosition(Vector3 mousePosition)
+    {
+        var boardPosition = GetClosestCellPosition(mousePosition);
+        if (!IsValidPosition(boardPosition))
+        {
+            return null;
+        }
+        return board.GetCell(boardPosition.x, boardPosition.y);
+    }
+
+    private bool IsValidPosition((int x, int y) position)
+    {
+        return position.x >= 0 && position.x < width &&
+               position.y >= 0 && position.y < height;
+    }
+
+    public (int x, int y) GetClosestCellPosition(Vector3 worldPos)
+    {
+        var threshold = boardItemSize * boardItemSize * 0.25f;
+        var closestCell = (-1, -1);
+        var minDist = float.MaxValue;
+
+        foreach (var item in boardItemViews)
+        {
+            var distSqr = ((Vector2)item.Value.transform.position - (Vector2)worldPos).sqrMagnitude;
+            if (distSqr < minDist)
+            {
+                minDist = distSqr;
+                closestCell = item.Key;
+            }
+        }
+
+        return minDist > threshold ? (-1, -1) : closestCell;
+    }
+
+    #endregion
+
+    #region Selection Handling
+
+    public void SetSelectedItems(Stack<BoardCell> selectedItems)
+    {
+        foreach (var item in selectedItems)
+        {
+            if (boardItemViews.TryGetValue(item.BoardPosition, out var view))
+            {
+                view.SetSelected();
+            }
+        }
+    }
+
+    public void ClearSelectedItems()
+    {
+        foreach (var view in boardItemViews.Values)
+        {
+            view.SetDeselected();
+        }
+    }
+
+    #endregion
+
+    #region Command Execution
+
+    public void ExecuteCommands(List<IBoardCommand> commands)
+    {
+        foreach (var command in commands)
+        {
+            command.Execute(this);
+        }
+    }
+
+    public void CreateBoardItem((int x, int y) position, IBoardItem boardItem)
+    {
+        CreateBoardItemViewAt(position.x, position.y, boardItem);
+    }
+
+    public void DestoryBoardItem((int x, int y) position)
+    {
+        if (boardItemViews.TryGetValue(position, out var view))
+        {
+            board.SetCell(position.x, position.y, new BoardCell(null, position));
+            PlayDestroyFeedback(view);
+            view.Factory.ReleaseObject(view);
+            boardItemViews.Remove(position);
+        }
+    }
+
+    public void MoveBoardItem((int x, int y) from, (int x, int y) to)
+    {
+        if (boardItemViews.TryGetValue(from, out var view))
+        {
+            view.FallTo(GetCellWorldPosition(to.x, to.y), 0f);
+            boardItemViews.Remove(from);
+            boardItemViews[to] = view;
+            board.SetCell(to.x, to.y, new BoardCell(view.BoardItem, to));
+        }
+    }
+
+    public void RefillBoard((int x, int y) finalPos, IBoardItem boardItem, float spawnY)
+    {
+        var view = CreateBoardItemViewAt(finalPos.x, finalPos.y, boardItem);
+        view.SetPosition(spawnY + finalPos.y * boardItemSize);
+        view.FallTo(GetCellWorldPosition(finalPos.x, finalPos.y), 0f);
+    }
+
+    public void CheckLinksRemaining()
+    {
+        if (!LinkCheckingSystem.CheckLinks(board))
+        {
+            ShuffleBoard();
+        }
+    }
+
+    #endregion
+
+    #region Feedback and Effects
+
+    private void PlayDestroyFeedback(BoardItemView view)
+    {
+        particleFeedback.feedbackColor = GetColorFromBoardItem(view.BoardItem);
+        feedbackPlayer.Play(view.transform.position);
+    }
+
+    public static Color GetColorFromBoardItem(IBoardItem boardItem) => boardItem.Color switch
+    {
+        BoardItemColor.Blue => Color.blue,
+        BoardItemColor.Green => Color.green,
+        BoardItemColor.Red => Color.red,
+        BoardItemColor.Yellow => Color.yellow,
+        _ => Color.white
+    };
 
     private void ShuffleBoard()
     {
@@ -84,160 +236,35 @@ public class BoardManager : MonoBehaviour, ICommandContext
         {
             var oldPos = results.oldPositions[i];
             var newPos = results.newPositions[i];
-            if (boardItemViews.TryGetValue(oldPos, out var boardItemView))
+            if (boardItemViews.TryGetValue(oldPos, out var oldView))
             {
-                var newBoardItemView = boardItemViews[newPos];
-                boardItemViews.Remove(oldPos);
-                boardItemViews.Remove(newPos);
-                boardItemViews[oldPos] = newBoardItemView;
-                boardItemViews[newPos] = boardItemView;
-                board.SetCell(oldPos.Item1, oldPos.Item2, new BoardCell(newBoardItemView.BoardItem, oldPos));
-                board.SetCell(newPos.Item1, newPos.Item2, new BoardCell(boardItemView.BoardItem, newPos));
-                boardItemView.MoveTo(GetCellWorldPosition(newPos.Item1, newPos.Item2), moveTime);
-                newBoardItemView.MoveTo(GetCellWorldPosition(oldPos.Item1, oldPos.Item2), moveTime);
+                var newView = boardItemViews[newPos];
+                UpdateBoardItemPositions(oldPos, newPos, oldView, newView, moveTime);
             }
         }
     }
 
-    internal Vector3 GetCellWorldPosition(int x, int y)
+    private void UpdateBoardItemPositions((int, int) oldPos, (int, int) newPos,
+        BoardItemView oldView, BoardItemView newView, float moveTime)
     {
-        //-y on z axis for 2D view
-        return new Vector3(x * boardItemSize - xOffset, y * boardItemSize - yOffset, -y);
+        boardItemViews.Remove(oldPos);
+        boardItemViews.Remove(newPos);
+        boardItemViews[oldPos] = newView;
+        boardItemViews[newPos] = oldView;
+
+        board.SetCell(oldPos.Item1, oldPos.Item2, new BoardCell(newView.BoardItem, oldPos));
+        board.SetCell(newPos.Item1, newPos.Item2, new BoardCell(oldView.BoardItem, newPos));
+
+        oldView.MoveTo(GetCellWorldPosition(newPos.Item1, newPos.Item2), moveTime);
+        newView.MoveTo(GetCellWorldPosition(oldPos.Item1, oldPos.Item2), moveTime);
     }
 
-    public BoardCell? GetBoardCellFromMousePosition(Vector3 mousePosition)
+    private IEnumerator DelayRoutine(float delayAmount, Action action)
     {
-        var boardPosition = GetClosestCellPosition(mousePosition);
-        if (boardPosition.x < 0 || boardPosition.x >= width || boardPosition.y < 0 || boardPosition.y >= height)
-        {
-            return null;
-        }
-
-        return board.GetCell(boardPosition.x, boardPosition.y);
-    }
-
-    public Board GetBoard()
-    {
-        return board;
-    }
-
-
-    public (int x, int y) GetClosestCellPosition(Vector3 worldPos)
-    {
-        (int, int) closestCell = (-1, -1);
-        var minDist = float.MaxValue;
-        var threshold = boardItemSize * boardItemSize * 0.25f;
-
-        foreach (var item in boardItemViews)
-        {
-            var cubePos = item.Value.transform.position;
-            var distSqr = ((Vector2)cubePos - (Vector2)worldPos).sqrMagnitude;
-            if (distSqr < minDist)
-            {
-                minDist = distSqr;
-                closestCell = item.Key;
-            }
-        }
-
-        if (minDist > threshold)
-        {
-            return (-1, -1);
-        }
-
-        return closestCell;
-    }
-
-    public void SetSelectedItems(Stack<BoardCell> selectedItems)
-    {
-        foreach (var item in selectedItems)
-        {
-            var position = item.BoardPosition;
-            if (boardItemViews.TryGetValue(position, out var boardItemView))
-            {
-                boardItemView.SetSelected();
-            }
-        }
-    }
-
-    public void ClearSelectedItems()
-    {
-        foreach (var item in boardItemViews)
-        {
-            item.Value.SetDeselected();
-        }
-    }
-
-    public static Color GetColorFromBoardItem(IBoardItem boardItem)
-    {
-        switch (boardItem.Color)
-        {
-            case BoardItemColor.Blue:
-                return Color.blue;
-            case BoardItemColor.Green:
-                return Color.green;
-            case BoardItemColor.Red:
-                return Color.red;
-            case BoardItemColor.Yellow:
-                return Color.yellow;
-            default:
-                return Color.white;
-        }
-    }
-
-    public void ExecuteCommands(List<IBoardCommand> commands)
-    {
-        foreach (var command in commands)
-        {
-            command.Execute(this);
-        }
-    }
-
-    //-----------------------------Command context implementation--------------------------------
-
-    public void DestoryBoardItem((int x, int y) position)
-    {
-        if (boardItemViews.TryGetValue(position, out var boardItemView))
-        {
-            board.SetCell(position.x, position.y, new BoardCell(null, position));
-            particleFeedback.feedbackColor = GetColorFromBoardItem(boardItemView.BoardItem);
-            feedbackPlayer.Play(boardItemView.transform.position);
-            boardItemView.Factory.ReleaseObject(boardItemView);
-            boardItemViews.Remove(position);
-        }
-    }
-
-    public void MoveBoardItem((int x, int y) from, (int x, int y) to)
-    {
-        if (boardItemViews.TryGetValue(from, out var boardItemView))
-        {
-            var targetPosition = GetCellWorldPosition(to.x, to.y);
-            boardItemView.FallTo(targetPosition, 0f);
-            boardItemViews.Remove(from);
-            boardItemViews[to] = boardItemView;
-            board.SetCell(to.x, to.y, new BoardCell(boardItemView.BoardItem, to));
-        }
-    }
-
-    public void CreateBoardItem((int x, int y) position, IBoardItem boardItem)
-    {
-        CreateBoardItemViewAt(position.x, position.y, boardItem);
+        yield return new WaitForSeconds(delayAmount);
+        action?.Invoke();
     }
 
 
-    public void RefillBoard((int x, int y) finalPos, IBoardItem boardItem, float spawnY)
-    {
-        var boardItemView = CreateBoardItemViewAt(finalPos.x, finalPos.y, boardItem);
-        boardItemView.SetPosition(spawnY + finalPos.y * boardItemSize);
-        boardItemView.FallTo(GetCellWorldPosition(finalPos.x, finalPos.y), 0f);
-    }
-
-    public void CheckLinksRemaining()
-    {
-        var anyLinksRemaining = LinkCheckingSystem.CheckLinks(board);
-        if (!anyLinksRemaining)
-        {
-            ShuffleBoard();
-        }
-    }
-
+    #endregion
 }
